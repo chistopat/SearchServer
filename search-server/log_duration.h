b1ef9ba75b7f73708d8c048ed1931e102f7620fa
Egor Chistiakov <e.chistiakov@city-mobil.ru>
2022-02-26 14:02:55 +0300

    add log_duration.h

---
+++ search-server/log_duration.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+class LogDuration {
+public:
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration() = default;
+
+    explicit LogDuration(std::string message) : message_(std::move(message)) {}
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << message_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const Clock::time_point start_time_ = Clock::now();
+    std::string message_;
+};
+
+#define PROFILE_CONCAT_INTERNAL(X, Y) X ## Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
